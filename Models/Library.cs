6d6ac76d1da8ba00420343b8bfefc6a356ab679a
NoraKitchen <narwhaltortellini@gmail.com>
2016-06-07 17:41:00 -0600

    refactored -- methods moved to library class

--- Models/Library.cs
+++ Models/Library.cs
@@ -49,20 +49,117 @@ public class Library
         Console.WriteLine("2. Create Patron Account");
 
         var choice = Console.ReadLine();
-        if (choice == "1"){
+        if (choice == "1")
+        {
             Console.WriteLine("Please enter your name.");
             var name = Console.ReadLine();
             return FindPatron(name);
         }
-        else if (choice == "2"){
+        else if (choice == "2")
+        {
             Console.WriteLine("Please enter your name.");
             var name = Console.ReadLine();
             return CreatePatron(name);
         }
-        else{
+        else
+        {
             Console.WriteLine("Please enter a valid number.");
             Login();
             return null;
+        }
+    }
+
+    public void DonateBook()
+    {
+        Console.WriteLine("Enter the title of the book:");
+        var title = Console.ReadLine();
+
+        Console.WriteLine("Enter the author of the book:");
+        var author = Console.ReadLine();
+
+        var book = new Book(title, author);
+        Books.Add(book);
+
+        Console.WriteLine("Thank you!");
+    }
+
+    public void checkOut(Patron user)
+    {
+        if (Books.Count == 0)
+        {
+            Console.WriteLine("Our library is empty!  Please donate!");
+        }
+        else
+        {
+            LogCollection();
+            Console.WriteLine("Enter the number of the book you want to check out.");
+            var choice = Console.ReadLine();
+            //error thrown here if don't enter value, consider try/catch
+            {
+                try
+                {
+                    int index = int.Parse(choice);
+
+                    if (Books[index].CheckedOut)
+                    {
+                        Console.WriteLine("Sorry, this book is unavailable.");
+                    }
+                    else
+                    {
+                        user.BorrowedBooks.Add(Books[index]);
+                        Books[index].CheckedOut = true;
+                        Console.WriteLine(Books[index].CheckedOut);
+                    }
+                }
+                catch (System.FormatException)
+                {
+                    Console.WriteLine("Please enter a valid number!");
+                }
+            }
+        }
+    }
+
+    public void checkIn(Patron user)
+    {
+        if (Books.Count == 0)
+        {
+            Console.WriteLine("We don't have books in our catalogue!  Please donate!");
+        }
+        else
+        {
+            LogCollection();
+            Console.WriteLine("What is the number of the book you are checking in?");
+            var choice = Console.ReadLine();
+            //error thrown here if don't enter value, consider try/catch --System.FormatException
+            try
+            {
+                int index = int.Parse(choice);
+
+                foreach (var book in Books)
+                {
+                    if (book.CheckedOut)
+                    {
+                        if (book == Books[index])
+                        {
+                            user.BorrowedBooks.Remove(Books[index]);
+                            book.CheckedOut = false;
+                            Console.WriteLine(book.CheckedOut);
+                        }
+                        else if (Books.IndexOf(book) == Books.Count - 1)
+                        {
+                            Console.WriteLine("That book is not in our system.");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("That book has already been checked in!");
+                    }
+                }
+            }
+            catch (System.FormatException)
+            {
+                Console.WriteLine("Please enter a valid number!");
+            }
         }
     }
 
--- Program.cs
+++ Program.cs
@@ -8,92 +8,49 @@ namespace ConsoleApplication
         {
             var library = new Library();
             var currentUser = library.Login();
-            
+
             Start(library, currentUser);
 
         }
 
-        public static void Start(Library library, Patron user){
-                Console.WriteLine("Press 1 to donate a book, press 2 check in a book, 3 to check out a book, press 4 to log out, or press 5 to view your checked out items. Press Q to quit.");
-                var choice = Console.ReadLine();
+        public static void Start(Library library, Patron user)
+        {
+            Console.WriteLine("What would you like to do? Please enter a number.");
+            Console.WriteLine("1. Doante a book");
+            Console.WriteLine("2. Check in");
+            Console.WriteLine("3. Check out");
+            Console.WriteLine("4. View your checked out items");
+            Console.WriteLine("5. Log out");
+            Console.WriteLine("Press Q to quit.");
 
-                if (choice == "1"){
-                    donateBook(library);
-                }
-                else if (choice == "2"){
-                    checkIn(library, user);
-                } else if (choice == "3") {
-                    checkOut(library, user);
-                }
-                else if (choice =="4"){
-                    user = library.Login();
-                }
-                else if (choice == "5"){
-                    user.ViewItemsOut();
-                }
+            var choice = Console.ReadLine();
 
-            if (choice.ToLower() != "q"){
+            if (choice == "1")
+            {
+                library.DonateBook();
+            }
+            else if (choice == "2")
+            {
+                library.checkIn(user);
+            }
+            else if (choice == "3")
+            {
+                library.checkOut(user);
+            }
+            else if (choice == "4")
+            {
+                user.ViewItemsOut();
+            }
+            else if (choice == "5")
+            {
+                user = library.Login();
+            }
+
+            if (choice.ToLower() != "q")
+            {
                 Start(library, user);
             }
         }
 
-
-        public static void donateBook(Library library){
-            Console.WriteLine("Enter the title of the book:");
-            var title = Console.ReadLine();
-
-            Console.WriteLine("Enter the author of the book:");
-            var author = Console.ReadLine();
-
-            var book = new Book(title, author);
-            library.Books.Add(book);
-
-            Console.WriteLine("Thank you!");
-        }
-
-        public static void checkOut(Library library, Patron user) {
-            if (library.Books.Count == 0) {
-                Console.WriteLine("Our library is empty!  Please donate!");
-            } else {
-                library.LogCollection();
-                Console.WriteLine("Enter the number of the book you want to check out.");
-                var choice = Console.ReadLine();
-                int index = int.Parse(choice);
-
-                if(library.Books[index].CheckedOut) {
-                    Console.WriteLine("Sorry, this book is unavailable.");
-                } else {
-                    user.BorrowedBooks.Add(library.Books[index]);
-                    library.Books[index].CheckedOut = true;
-                    Console.WriteLine(library.Books[index].CheckedOut);
-                }
-            }
-            
-        }
-
-        public static void checkIn(Library library, Patron user) {
-            if (library.Books.Count == 0) {
-                Console.WriteLine("We don't have books in our catalogue!  Please donate!");
-            } else {
-                library.LogCollection();
-                Console.WriteLine("What is the number of the book you are checking in?");
-                var choice = Console.ReadLine();
-                int index = int.Parse(choice);
-
-                foreach(var book in library.Books) {
-                    if (book.CheckedOut) {
-                        if (book == library.Books[index]) {
-                            user.BorrowedBooks.Remove(library.Books[index]);
-                            book.CheckedOut = false;
-                            Console.WriteLine(book.CheckedOut);
-                        } else if(library.Books.IndexOf(book) == library.Books.Count-1) {
-                            Console.WriteLine("That book is not in our system.");
-                        }
-                    } else {
-                        Console.WriteLine("That book has already been checked in!");
-                    }
-                }
-            }
-        }
     }
 }
